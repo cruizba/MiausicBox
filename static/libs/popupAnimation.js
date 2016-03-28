e8ce2d4a2ee9ad7ba6949ff486e5a37c4cd42f6c
Thais <lintu.yin@gmail.com>
2016-03-28 23:49:23 +0200

    pop ups mensajes y cambios varios

--- static/libs/popupAnimation.js
+++ static/libs/popupAnimation.js
@@ -13,8 +13,38 @@
     });
     
     $(function(){
+       $('#passButton').bind('click', function(event){
+           popupForm(event, "#formularioContraseña");
+        });
+    });
+    
+    $(function(){
        $('#crearBandaButton').bind('click', function(event){
            popupForm(event, "#formularioCrearBanda");
+        });
+    });
+    
+    $(function(){
+       $('#writeButton').bind('click', function(event){
+           popupForm(event, "#formularioRedactar");
+        });
+    });
+    
+    $(function(){
+       $('#sendButton').bind('click', function(event){
+           popupForm(event, "#formularioReenviar");
+        });
+    });
+    
+    $(function(){
+       $('#answerButton').bind('click', function(event){
+           popupForm(event, "#formularioResponder");
+        });
+    });
+    
+    $(function(){
+       $('#entrarButton').bind('click', function(event){
+           popupForm(event, "#formularioLogin");
         });
     });
     
