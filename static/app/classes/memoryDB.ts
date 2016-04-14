590dec322bd765b287ba41f8a6d54f8a22535ca4
Carlos Ruiz Ballesteros <c.ruizbal94@gmail.com>
2016-04-14 18:21:54 +0200

    Pequeños cambios

--- static/app/classes/memoryDB.ts
+++ static/app/classes/memoryDB.ts
@@ -3,7 +3,7 @@ import { User } from './User'
 export var userList:User[] = [
 {userName:'Mica',
 completeName:'Micael Gallego',
-description:'Soy hipster y profesor como hobbie',
+description:'Profesor URJC',
 isArtist: false,
 city:'Móstoles',
 instrument:'Portátil'}
