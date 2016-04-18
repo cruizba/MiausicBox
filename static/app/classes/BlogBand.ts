f4d9bcfc45ced74352b1fb115f7e6a0e2c199d51
ice6294 <luigi6294@gmail.com>
2016-04-18 20:03:54 +0200

    Classes Blog, BlogUser, BlogBand, Event, Follow, Message added

---
+++ static/app/classes/BlogBand.ts
@@ -0,0 +1,43 @@
+/**
+ *  Class that represent the information related to MiausicBox users
+ *  @class BlogBand
+ */
+import { Blog } from './Blog';
+import { Band } from './User';
+
+export class BlogBand extends Blog {
+
+  /* Attributes */
+  private _author:Band;
+
+  /* Constructor */
+  constructor(name:string, image:string, text:string, date:Date, author:Band) {
+    super(name, image, text, date);
+    this._author = author;
+  }
+
+  /* Getters & Setters */
+  get author():Band {
+    return this._author;
+  }
+
+  set author(author:Band) {
+    this._author = author;
+  }
+
+  /** Return if an BlogBand is equal to other
+   *  @method equals
+   *  @param {object} object
+   */
+  equals(object:any){
+    //At ApiRest object, this comparaison should be the with both ids
+    if (!(object instanceof BlogBand)) {
+      return false;
+    } else {
+      var blog:BlogBand = object;
+      return (super.equals &&
+              blog.author == this.author);
+    }
+  }
+
+}
---
+++ static/app/classes/Follow.ts
@@ -0,0 +1,51 @@
+/**
+ * Class that represent the information related to MiausicBox users
+ * @class Follow
+ */
+import { User } from './User';
+
+export class Follow {
+
+  /* Attributes */
+  private _emisor:User;
+  private _receptor:User;
+
+  /* Constructor */
+  constructor(emisor:User, receptor:User) {
+    this._emisor = emisor;
+    this._receptor = receptor;
+  }
+
+  /* Getters & Setters */
+  get emisor():User {
+    return this.emisor;
+  }
+
+  get receptor():User {
+    return this._receptor;
+  }
+
+  set emisor(emisor:User) {
+    this._emisor = emisor;
+  }
+
+  set receptor(receptor:User) {
+    this._receptor = receptor;
+  }
+
+  /** Return if an Follow is equal to other
+   *  @method equals
+   *  @param {object} object
+   */
+  equals(object:any){
+    //At ApiRest object, this comparaison should be the with both ids
+    if (!(object instanceof Follow)) {
+      return false;
+    } else {
+      var follow:Follow = object;
+      return (follow.emisor == this.emisor &&
+              follow.receptor == this.receptor);
+    }
+  }
+
+}
