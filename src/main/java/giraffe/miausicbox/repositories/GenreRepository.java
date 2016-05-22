15cf81fa90d155d1565aa95be00aeaf937bf4aee
ice6294 <luigi6294@gmail.com>
2016-05-22 21:49:28 +0200

    API-Rest> Generic controller agregado, creados las entidades genre e instrument

---
+++ src/main/java/giraffe/miausicbox/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package giraffe.miausicbox.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import giraffe.miausicbox.model.Genre;
+
+public interface GenreRepository extends JpaRepository<Genre, Integer> {
+
+}
